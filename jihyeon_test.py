c5a9ef71672a373a65906d9d1e43ffcf04d96f37
지현 <57143818+spy03128@users.noreply.github.com>
2020-12-09 04:05:46 +0900

    x

---
+++ jihyeon_test.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import pymysql
+import folium
+from folium import plugins
+from folium.plugins import HeatMap
+print(folium.__version__)
+
+conn = pymysql.Connect(host="localhost", user="root", password="wlsdud369", db="db")
+curs = conn.cursor()
+
+sql = "select * from city_corona_db"
+try:
+    curs.execute(sql)
+    results = curs.fetchall()
+    discresults = {}
+    for row in results:
+        City_name = row[0]
+        Total = row[1]
+        Domestic_Occurrence = row[2]
+        Foreign_Inflow = row[3]
+        Confirmed_Ratient = row[4]
+        Quarantine = row[5]
+        Isolation_Release = row[6]
+        Dead = row[7]
+        Incident_rate = row[8]
+
+except:
+    print('ERROR!!')
+
+m = folium.Map(location=[48, -102], zoom_start = 3)
+
+
+conn.close()
+
+m
